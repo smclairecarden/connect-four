dbafdf3a85d630ba8073655945646fa4777d28ae
Claire Carden <smclairecarden@gmail.com>
2022-02-10 16:26:44 -0600

    board circles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,55 @@
-console.log('sanity check')
+/*-------------------------------- Constants --------------------------------*/
+
+winningCombos = []
+
+
+let grid = []
+
+const player1 = 1
+const player2 = -1
+let  nextTurn, T
+
+/*------------------------ Cached Element References ------------------------*/
+
+const board = document.querySelector('.container')
+const theSquares = document.querySelectorAll('.grid-area')
+const startGame = document.querySelector('#start-game')
+const resetGame = document.querySelector('#reset-game')
+
+
+
+/*-------------------------------- Event Listeners --------------------------------*/
+
+// theSquares.forEach((square, idx) => {
+//   square.addEventListener('click', handleClick)
+// })
+
+board.addEventListener('click', handleClick)
+
+/*-------------------------------- Functions --------------------------------*/
+
+function init() {
+  boardSquares = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+  nextTurn = 1
+  T = 'tie'
+  render()
+}
+init()
+
+function render() {
+  switchTurn()
+}
+
+function handleClick(evt) {
+const i = (evt.target.id)
+if(boardSquares[i] === null) {
+  boardSquares[i] = nextTurn
+  render()
+}
+console.log(i)
+}
+
+
+function switchTurn() {
+  nextTurn *= -1
+}
